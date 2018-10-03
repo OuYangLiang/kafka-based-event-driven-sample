897338d0410e415e0634c25bfebf15d7cc4d44e4
OuYang Liang <ouyanggod@gmail.com>
2018-10-03 22:13:03 +0800

    事件消端实现幂等。

--- src/main/java/com/personal/oyl/event/web/subscribers/UserOrderReportSubscriber.java
+++ src/main/java/com/personal/oyl/event/web/subscribers/UserOrderReportSubscriber.java
@@ -1,38 +1,59 @@
 package com.personal.oyl.event.web.subscribers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.personal.oyl.event.BaseSubscriber;
 import com.personal.oyl.event.Event;
+import com.personal.oyl.event.EventMapper;
 import com.personal.oyl.event.order.Order;
 import com.personal.oyl.event.order.UserOrderReport;
 import com.personal.oyl.event.order.OrderRepos;
 
-@Component
+@Component("userOrderReportSubscriber")
 public class UserOrderReportSubscriber implements BaseSubscriber {
+
+    private static final Logger log = LoggerFactory.getLogger(UserOrderReportSubscriber.class);
     
     @Autowired
     private OrderRepos repos;
-
+    
+    @Autowired
+    private EventMapper eventMapper;
+    
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED, rollbackFor = Exception.class)
     @Override
     public void onEvent(Event e) {
-        Order order = Order.fromJson(e.getContext());
-        UserOrderReport report = repos.selectUserOrderReportByKey(order.getUserId());
         
-        if (null == report) {
-            report = new UserOrderReport();
-            report.setUserId(order.getUserId());
-            report.setOrderNum(1l);
-            report.setOrderTotal(new Long(order.getOrderAmount()));
+        try {
+            Order order = Order.fromJson(e.getContext());
+            UserOrderReport report = repos.selectUserOrderReportByKey(order.getUserId());
             
-            repos.createUserOrderReport(report);
-        } else {
-            report.setOrderNum(report.getOrderNum() + 1);
-            report.setOrderTotal(report.getOrderTotal() + order.getOrderAmount());
+            if (null == report) {
+                report = new UserOrderReport();
+                report.setUserId(order.getUserId());
+                report.setOrderNum(1l);
+                report.setOrderTotal(new Long(order.getOrderAmount()));
+                
+                repos.createUserOrderReport(report);
+            } else {
+                report.setOrderNum(report.getOrderNum() + 1);
+                report.setOrderTotal(report.getOrderTotal() + order.getOrderAmount());
+                
+                repos.updateUserOrderReport(report);
+            }
             
-            repos.updateUserOrderReport(report);
+            eventMapper.archive("000000000001", e);
+        } catch (DuplicateKeyException ex) {
+            log.warn("Duplicated message " + e.json());
         }
+        
     }
 
 }
